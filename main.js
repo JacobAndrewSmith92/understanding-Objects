280a01786fa53561741dc70cf1f1888a387b418d
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-04-06 11:52:09 -0500

    Created objects for platform statements(taxes, jobs, infrastructure, healthcare, crime and reinforcement), congressional district #, donation form & url, and calendar of events with empty array to hold event dates.

--- main.js
+++ main.js
@@ -68,3 +68,194 @@ const expense = Object.create({}, {
 })
 
 // By default, use Object.create 
+
+
+/*
+
+You have volunteered your time to a local political candidate, Elizabeth Sanger, who wants to become a US representative in Congress for your district. Unfortunately, the team discovered that you're a software developer, so they have begged you to build an application that lets them track volunteers, and store information about Elizabeth and her campaign.
+
+Your job is to define the different objects and resources, their structure, and the corresponding properties for each, to represent the following information about Elizabeth's campaign.
+
+Her congressional district (you can use yours here)
+Her platform statements for the following issues.
+    Taxes
+    Jobs
+    Infrastructure
+    Health care
+    Crime and enforcement
+URL for donation form
+Calendar of events
+Volunteer information
+    Name
+    Address
+    Email
+    Phone number
+    Availability
+    What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
+Biography
+Image gallery
+    Head shot
+    Picture of family
+    Picture of constituents
+Mission statement
+URL for registering to vote
+
+*/ 
+
+const congressionalDistrict = Object.create({}, {
+    district: {
+        enumerable: true,
+        writable: false,
+        value: "5th"
+    }
+})
+
+const platformStatements = Object.create({}, {
+    taxes: {
+        enumerable: true,
+        writable: false,
+        value: "Profound statement on taxes - Lorem ipsum dolor amet gluten-free fingerstache dreamcatcher blue bottle cronut synth. Stumptown la croix lo-fi banh mi drinking vinegar microdosing portland fixie air plant. Tilde af pitchfork, XOXO shoreditch live-edge succulents copper mug gastropub master cleanse biodiesel farm-to-table. Keytar normcore selvage aesthetic food truck vexillologist. Forage fam lyft listicle leggings tattooed biodiesel bicycle rights street art pinterest keffiyeh locavore celiac retro unicorn. Jianbing celiac actually tbh freegan tumeric."
+    },
+    jobs: {
+        enumerable: true,
+        writable: false,
+        value: "Profound statement on jobs - Lorem ipsum dolor amet cold-pressed drinking vinegar butcher cronut, you probably haven't heard of them wayfarers fingerstache la croix blue bottle activated charcoal. Taxidermy salvia beard gastropub unicorn craft beer asymmetrical cloud bread normcore pabst cliche tbh four loko. Butcher pabst coloring book meh cardigan palo santo wolf iPhone blog sriracha cloud bread fam ramps."
+    }, 
+    infrastructure: {
+        enumerable: true,
+        writable: false,
+        value: "Profound statement on infrastructure - Bitters mustache artisan mixtape ennui hoodie succulents copper mug actually vexillologist af venmo ramps flannel bushwick. Art party ethical hella, iPhone edison bulb la croix keytar chicharrones meditation four loko pok pok kickstarter."
+    }, 
+    healthCare: {
+        enumerable: true,
+        writable: false,
+        value: "Profound statement on healthcare - Etsy gastropub chambray drinking vinegar, next level la croix waistcoat leggings raw denim godard PBR&B tofu four loko put a bird on it. Tofu ugh live-edge, next level marfa wayfarers thundercats drinking vinegar chicharrones williamsburg YOLO paleo."
+    },
+    crimeAndReinforcement: {
+        enuemrable: true,
+        writable: false,
+        value: "Profound statement on crime and reinforcement - Succulents schlitz hot chicken, mumblecore affogato 90's cold-pressed thundercats poutine roof party irony vegan cred. Farm-to-table tumblr forage, drinking vinegar skateboard pour-over pork belly vice wayfarers dreamcatcher scenester vaporware disrupt synth. Seitan master cleanse gluten-free, cardigan ennui street art adaptogen twee 8-bit kinfolk semiotics jean shorts vexillologist." 
+    }
+})
+
+const urlForDonationsForms = Object.create({}, {
+    url: {
+        enumerable: true, 
+        writable: false,
+        value: "www.mywebsitefordistrict.com"
+    }
+})
+
+const calendarOfEvents = [];
+
+const events = Object.create({}, {
+    eventOne: {
+        dateCreated: {
+            enumerable: true,
+            writable: false,
+            value: "10/10/18"
+        },
+        venue: {
+            enumerable: true,
+            writable: false,
+            value: "Nashville Software School"
+        },
+        demographic: {
+            enumerable: true,
+            writable: false,
+            value: "Developers"
+        },
+        platformStatement: {
+            enumerable: true,
+            writable: false,
+            value: platformStatements.jobs.value
+        }
+    },
+    eventTwo: {
+        dateCreated: {
+            enumerable: true,
+            writable: false,
+            value: "10/12/18"
+        },
+        venue: {
+            enumerable: true,
+            writable: false,
+            value: ""
+        },
+        demographic: {
+            enumerable: true,
+            writable: false,
+            value: "Developers"
+        },
+        platformStatement: {
+            enumerable: true,
+            writable: false,
+            value: platformStatements.jobs.value
+        }
+    },
+    eventThree: {
+        dateCreated: {
+            enumerable: true,
+            writable: false,
+            value: "10/17/18"
+            },
+        venue: {
+            enumerable: true,
+            writable: false,
+            value: "White House"
+            },
+        demographic: {
+            enumerable: true,
+            writable: false,
+            value: "All groups"
+            },
+        platformStatement: {
+            enumerable: true,
+            writable: false,
+            value: platformStatements.value
+            }            
+    }
+})
+
+const volunteers = [];
+
+const volunteerInformation = Object.create({}, {
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
